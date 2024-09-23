1b648a9be174c488c29dbd1653dd7fdf56b94682
kienle.ag <kienle.ag@gmail.com>
2024-09-23 17:03:01 +0700

    modify dataset

--- write_dataset.py
+++ write_dataset.py
@@ -1,6 +1,15 @@
 import os
-lists = os.listdir('dataset/dataset/mfg')
+lists = os.listdir('dataset/dataset/date')
 lists.sort(key=lambda x: int(x.split('.')[0].split('_')[1]))
-with open('dataset/dataset/label_mfg.txt', 'w') as label_exp:
-    for item in lists:
-        label_exp.write(item + '\t\n')
+label_exp = []
+with open('dataset/dataset/label_date.txt', 'r', encoding='utf-8') as label_exp_read:
+    label_exp = label_exp_read.readlines()
+    label_exp = [label.strip() for label in label_exp]
+print(lists[:10])
+print(label_exp[:10])
+
+with open('dataset/dataset/label_date.txt', 'w', encoding='utf-8') as label_exp_write:
+    for index, label in enumerate(label_exp):
+        label_val = label.split('\t')[1]
+        file_name = lists[index]
+        label_exp_write.write(file_name + '\t' + label_val + '\n')
